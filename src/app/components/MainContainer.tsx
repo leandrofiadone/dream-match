3662ec7aaf244b809deaf1205e8d017bdf1b1fa0
Leandro <leanfiadone@gmail.com>
2024-08-09 18:41:10 -0300

    correcion codigo

--- src/app/components/MainContainer.tsx
+++ src/app/components/MainContainer.tsx
@@ -1,4 +1,28 @@
-// // /components/MainContainer.tsx
+
+import React from "react"
+
+const MainContainer: React.FC = () => {
+  return (
+    <div className="flex flex-col w-full  md:flex-row flex-1 gap-4 mt-5 mb-16 md:mb-5 ">
+      {/* Primer contenedor */}
+      <div className="flex-1 p-4 bg-gray-100 rounded-md shadow-md">
+        {/* Contenido del primer contenedor */}
+      </div>
+
+      {/* Contenedor central, visible solo en tablet y desktop */}
+      <div className="hidden md:flex md:flex-1 md:w-1/3 p-4 bg-gray-200 rounded-md shadow-md">
+        {/* Contenido del contenedor central */}
+      </div>
+
+      {/* Segundo contenedor */}
+      <div className="flex-1 p-4 bg-gray-100 rounded-md shadow-md">
+        {/* Contenido del segundo contenedor */}
+      </div>
+    </div>
+  )
+}
+
+export default MainContainer
 // import React from "react"
 
 // const MainContainer: React.FC = () => {
@@ -27,27 +51,4 @@
 
 
 // src/components/MainContainer.tsx
-import React from "react"
 
-const MainContainer: React.FC = () => {
-  return (
-    <div className="flex flex-col w-full  md:flex-row flex-1 gap-4 mt-5 mb-16 md:mb-5 ">
-      {/* Primer contenedor */}
-      <div className="flex-1 p-4 bg-gray-100 rounded-md shadow-md">
-        {/* Contenido del primer contenedor */}
-      </div>
-
-      {/* Contenedor central, visible solo en tablet y desktop */}
-      <div className="hidden md:flex md:flex-1 md:w-1/3 p-4 bg-gray-200 rounded-md shadow-md">
-        {/* Contenido del contenedor central */}
-      </div>
-
-      {/* Segundo contenedor */}
-      <div className="flex-1 p-4 bg-gray-100 rounded-md shadow-md">
-        {/* Contenido del segundo contenedor */}
-      </div>
-    </div>
-  )
-}
-
-export default MainContainer
