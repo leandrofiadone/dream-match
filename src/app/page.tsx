65a525b408c259e7ae34d661114f926beeb694b6
Leandro <leanfiadone@gmail.com>
2024-08-09 15:02:33 -0300

    overflow hidden

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import MainContainer from "./components/MainContainer"
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen overflow-hidden">
       <main className="flex-1 flex flex-col items-center justify-between p-4 md:p-9">
         <MainContainer />
       </main>
