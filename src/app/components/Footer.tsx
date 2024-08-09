03d134f404eab8aa832b8c3c8e467bfd02aa99d5
Leandro <leanfiadone@gmail.com>
2024-08-09 12:11:54 -0300

    diseño layout principal inicial

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,16 @@
+// src/components/Footer.tsx
+import Image from "next/image"
+
+export default function Footer() {
+  return (
+    <footer className="fixed bottom-0 left-0 w-full bg-gray-800 text-white p-4 md:hidden flex justify-around">
+      {/* Footer content */}
+     
+
+
+        <span className="block text-xs">ATC - Dream Match</span>
+
+
+    </footer>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from "next/image";
+import Footer from './components/Footer';
+
+ // Asegúrate de que la ruta sea correcta
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-4 md:p-9">
+      {/* Tu contenido principal aquí */}
 
-
+      {/* Footer */}
+      <Footer />
     </main>
-  );
+  )
 }
