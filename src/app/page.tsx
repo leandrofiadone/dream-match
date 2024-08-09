c67e705f0bd4fe44ec3bb98f404ae72d5a0180ff
Leandro <leanfiadone@gmail.com>
2024-08-09 15:11:25 -0300

    responsive container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import MainContainer from "./components/MainContainer"
 
 const Home: React.FC = () => {
   return (
-    <div className="flex flex-col min-h-screen overflow-hidden">
-      <main className="flex-1 flex flex-col items-center justify-between p-4 md:p-9">
+    <div className="flex flex-col min-h-full">
+      <main className="flex-1 flex flex-col absolute inset-0 items-center justify-between p-4 md:p-9">
         <MainContainer />
       </main>
       <Footer />
