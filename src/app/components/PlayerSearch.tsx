df8cb7771356a469a13d5b01f6dc4d933cc78b7c
Leandro <leanfiadone@gmail.com>
2024-08-12 20:02:18 -0300

    mas responsive

--- src/app/components/PlayerSearch.tsx
+++ src/app/components/PlayerSearch.tsx
@@ -71,7 +71,7 @@ const handlePlayerClick = (player: Player) => {
   onPlayerSelect(
     <div className="flex">
       {player.player_image ? (
-        <div className="w-9 h-9 sm:w-14 sm:h-14 sm:mt-auto sm:mb-3 sm:ml-3 ml-5 relative rounded-full">
+        <div className="w-9 h-9 sm:w-14 sm:h-14  sm:mb-3 sm:ml-3 ml-5 relative rounded-full">
           <Image
             src={player.player_image}
             alt={player.player_name}
@@ -82,18 +82,18 @@ const handlePlayerClick = (player: Player) => {
           />
         </div>
       ) : (
-        <div className="flex justify-center items-center w-9 h-9 ml-3 bg-gray-200 rounded-full">
+        <div className="flex justify-center items-center w-9 h-9 sm:ml-3 ml-5 bg-gray-200 rounded-full">
           <FaUserAlt size={24} className="text-gray-500" />
         </div>
       )}
 
-      <div className="flex flex-col sm:mr-auto  mr-6 ml-auto ">
-        <h2 className="text-sm font-semibold">
+      <div className="flex flex-col sm:text-center   mr-6 sm:mr-auto ml-auto ">
+        <h2 className="text-sm   font-semibold">
           {player.player_name} ({player.player_number})
         </h2>
 
         <div className="flex flex-col sm:text-center text-right ">
-          <p className=" text-xs text-gray-600">Team: {player.player_team}</p>
+          <p className="text-xs text-gray-600">Team: {player.player_team}</p>
           <div className="hidden sm:block ">
             <p className="text-xs text-gray-600">
               Goals: {player.player_goals}
