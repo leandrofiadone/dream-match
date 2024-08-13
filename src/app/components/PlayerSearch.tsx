8a7b4583f7a36df3252847e293011ccaa0995d83
Leandro <leanfiadone@gmail.com>
2024-08-12 22:33:01 -0300

    mejoras diseño

--- src/app/components/PlayerSearch.tsx
+++ src/app/components/PlayerSearch.tsx
@@ -71,7 +71,7 @@ const handlePlayerClick = (player: Player) => {
   onPlayerSelect(
     <div className="flex">
       {player.player_image ? (
-        <div className="w-9 h-9 sm:w-14 sm:h-14  sm:mb-3 sm:ml-3 ml-5 relative rounded-full">
+        <div className="w-9 h-9 sm:w-14 sm:h-14 lg:ml-5 lg:w-18 lg:h-18 sm:mb-3 sm:ml-3 ml-5 relative rounded-full">
           <Image
             src={player.player_image}
             alt={player.player_name}
@@ -88,20 +88,24 @@ const handlePlayerClick = (player: Player) => {
       )}
 
       <div className="flex flex-col sm:text-center   mr-6 sm:mr-auto ml-auto ">
-        <h2 className="text-sm   font-semibold">
+        <h2 className="xl:text-base text-sm   font-semibold">
           {player.player_name} ({player.player_number})
         </h2>
+        <div className="flex flex-col sm:text-center text-right lg:flex-row lg:justify-between">
+          <div className="lg:w-1/2 lg:mr-4">
+            <p className="text-xs lg:text-base text-gray-600">
+              Team: {player.player_team}
+            </p>
+          </div>
 
-        <div className="flex flex-col sm:text-center text-right ">
-          <p className="text-xs text-gray-600">Team: {player.player_team}</p>
-          <div className="hidden sm:block ">
-            <p className="text-xs text-gray-600">
+          <div className="hidden sm:block lg:w-1/2">
+            <p className="text-xs lg:text-sm text-gray-600">
               Goals: {player.player_goals}
             </p>
-            <p className="text-xs text-gray-600">
+            <p className="text-xs lg:text-sm text-gray-600">
               Assists: {player.player_assists}
             </p>
-            <p className="text-xs text-gray-600">
+            <p className="text-xs lg:text-sm text-gray-600">
               Rating: {player.player_rating}
             </p>
           </div>
