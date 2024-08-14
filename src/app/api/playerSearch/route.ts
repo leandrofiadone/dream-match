087534848ba46690d2b0095c783dc42f6022944a
Leandro <leanfiadone@gmail.com>
2024-08-13 21:53:07 -0300

    buscador anda por rating

--- src/app/api/playerSearch/route.ts
+++ src/app/api/playerSearch/route.ts
@@ -3,22 +3,49 @@ import axios from "axios"
 
 export async function GET(request: NextRequest) {
   const {searchParams} = new URL(request.url)
-  const playerName = searchParams.get("playerName")
+  const playerName = searchParams.get("playerName")?.toLowerCase().trim() || ""
 
   try {
     const response = await axios.get(
       `https://apiv3.apifootball.com/?action=get_players&player_name=${playerName}&APIkey=${process.env.SECRET_API_KEY}`
     )
 
-    // Ordenar jugadores por rating (siendo los sin rating al final)
-    const players = response.data.sort((a: any, b: any) => {
+    // Dividir el nombre de búsqueda en partes
+    const nameParts = playerName.split(" ")
+
+    // Refinar la búsqueda con logs adicionales
+    const players = response.data.filter((player: any) => {
+      const shortName = player.player_name?.toLowerCase().trim() || ""
+      const fullName = player.player_complete_name?.toLowerCase().trim() || ""
+
+      const matchFound = nameParts.some(
+        (part) => shortName.includes(part) || fullName.includes(part)
+      )
+
+      // Log detallado para cada jugador
+      console.log({
+        searchParts: nameParts,
+        shortName,
+        fullName,
+        matchFound
+      })
+
+      return matchFound
+    })
+
+    // Ordenar jugadores por rating
+    players.sort((a: any, b: any) => {
       const ratingA = parseFloat(a.player_rating) || 0
       const ratingB = parseFloat(b.player_rating) || 0
       return ratingB - ratingA
     })
 
+    // Log del primer jugador encontrado
+    console.log("First player found:", players[0])
+
     return new Response(JSON.stringify(players), {status: 200})
   } catch (error) {
+    console.error("Error fetching player data:", error)
     return new Response(JSON.stringify({error: "Error fetching player data"}), {
       status: 500
     })
