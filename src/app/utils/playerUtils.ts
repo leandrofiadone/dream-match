cd8a7c94807ec1ba4f2625d7ef7a121ac2d18f0b
Leandro <leanfiadone@gmail.com>
2024-08-14 14:06:46 -0300

    avanti2024

--- src/app/utils/playerUtils.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios"
-import {Player} from "../types/types"
-
-export const fetchPlayerData = async (
-  playerName: string
-): Promise<Player[]> => {
-  const response = await axios.get(`/api?search=${playerName}`)
-  return response.data.player ?? []
-}
